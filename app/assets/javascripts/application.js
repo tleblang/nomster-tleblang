5acab97c64834a7f92bb0eaf614654467bac70fc
T LeBlang <thanya2029@gmail.com>
2019-06-29 06:56:09 +0000

    update gemfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= requir jquery
+//= requir jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
