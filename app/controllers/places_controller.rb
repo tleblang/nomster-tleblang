97ee08adf531807c21acf7e105792ba4d24e6393
T LeBlang <thanya2029@gmail.com>
2019-07-01 21:10:23 +0000

    connect users places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,7 +9,7 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
 	private 
