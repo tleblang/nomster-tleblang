b66e8ef1a6aa872ff6c8723c647d1e00094e724f
T LeBlang <thanya2029@gmail.com>
2019-07-02 05:57:13 +0000

    show, edit, update and destryo

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create]
 	
 	def index
   	@places = Place.paginate(page: params[:page], per_page: 4)
@@ -12,9 +12,32 @@ class PlacesController < ApplicationController
 		current_user.places.create(place_params)
 		redirect_to root_path
 	end
+
+	def show
+		@place = Place.find(params[:id])
+	end
+
+	def edit
+		@place = Place.find(params[:id])
+	end
+
+	def update
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
+	end
+
+	def destroy
+  	@place = Place.find(params[:id])
+  	@place.destroy
+  	redirect_to root_path
+	end 
+
 	private 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
 	end
+
+
 
 end
