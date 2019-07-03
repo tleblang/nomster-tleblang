15bc48de3d1491346ba991ab459dbd59f99a04fa
T LeBlang <thanya2029@gmail.com>
2019-07-03 02:24:38 +0000

    add map to place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,16 @@
 class PlacesController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+
 	def index
   	@places = Place.paginate(page: params[:page], per_page: 4)
 	end
+
+
 	def new
 		@place = Place.new
 	end
+
 
 	def create
 		@place = current_user.places.create(place_params)
@@ -17,9 +21,11 @@ class PlacesController < ApplicationController
 		end
 	end
 
+
 	def show
 		@place = Place.find(params[:id])
 	end
+
 
 	def edit
 		@place = Place.find(params[:id])
@@ -28,6 +34,7 @@ class PlacesController < ApplicationController
 			return render plain: 'Not Allowed', status: :forbidden
 		end
 	end
+
 
 	def update
 		@place = Place.find(params[:id])
@@ -44,6 +51,7 @@ class PlacesController < ApplicationController
 		end
 	end
 
+
 	def destroy
   	@place = Place.find(params[:id])
   	if @place.user != current_user
@@ -53,11 +61,10 @@ class PlacesController < ApplicationController
   	redirect_to root_path
 	end 
 
+
 	private 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
 	end
-
-
 
 end
